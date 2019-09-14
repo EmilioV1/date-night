53e03b9242f6d71c1fa118b230f9e4e702f60a49
Tellis <teellis20@gmail.com>
2019-09-14 11:27:53 -0700

    yelp api added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,41 @@
-// API call
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// name, rating, price, image, address and url
 
 // 'Submit' button on click event
 $(".submit-btn").on("click", function() {
 
-    var input = $("#input-food").val().trim();
-    var location= $("#input-address").val().trim();
+    // API call
+var location = $("#input-address").val().trim();
+var input = $("#input-food").val().trim();
+var queryUrl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?limit=40&location=" + location + "&term=" + input
+
+var restaurants =[];
+var selectedRestaurant;
+
+$.ajax({
+    url: queryUrl,
+    method: "GET",
+    contentType: "text/plain",
+    headers: {
+        "Authorization": "Bearer ywecqt0W3LheBcZHlrPN7UHTFXnNV0qEO6IUCGYoLDQYlhRcLMRv8XExahKsYaSJf5z5lvb0x0X4ox1Go-PdDX4oRf99SMrEVpbDwrpkiKPsWrTZpdDGL_VX0LZ5XXYx"
+    },
+   }).then(function(response){
+    
+    // console.log(response);
+    for ( var i = 0; i < response.businesses.length; i++) {
+    restaurants.push(response.businesses[i])
+    };
+    console.log(restaurants[selectedRestaurant].name);
+    console.log(restaurants[selectedRestaurant].rating);
+    console.log(restaurants[selectedRestaurant].location.address1);
+    console.log(restaurants[selectedRestaurant].price);
+    console.log(restaurants[selectedRestaurant].image_url);
+    console.log(restaurants[selectedRestaurant].url);
+    
+    
+})
+
+selectedRestaurant = Math.floor(Math.random() * 40);
+
     var div = $("<div class='card card-2 center'>");
     var content = $("<div class=\"section col s12 m6\">" + input + "</div>")
     var content2 = $("<div class=\"section col s12 m6\">" + input + "</div>")
